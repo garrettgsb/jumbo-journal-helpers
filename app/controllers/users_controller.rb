b3444a9043ca9a4a1776eeb220d02529ead961e4
Garrett <garrettgsb@gmail.com>
2019-01-15 23:18:20 -0800

    Add functionality and view for creating new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token # Don't do this.
+
   def index
     @users = User.all
     render :index
@@ -8,5 +10,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @journals = @user.journals
     render :show
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.create name: params[:name], password: params[:password]
+    redirect_to "/users/#{@user.id}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root to: 'application#home'
 
   get '/users', to: 'users#index'
+  get '/users/new', to: 'users#new'
   get '/users/:id', to: 'users#show'
+
+  post '/users', to: 'users#create'
 end
