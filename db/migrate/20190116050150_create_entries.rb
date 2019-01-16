1cde56a3a743280bf3a1da098c44ffe7ef08f3ae
Garrett <garrettgsb@gmail.com>
2019-01-15 21:19:26 -0800

    Models and associations for User, Journal, and Entry

---
+++ db/migrate/20190116050150_create_entries.rb
@@ -0,0 +1,12 @@
+class CreateEntries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :entries do |t|
+      t.string :title
+      t.text :body
+      t.references :journal, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
